d4c371c9d27e6bb924b387cebc0ee8155984869b
BalasaiTerli <138983969+BalasaiTerli@users.noreply.github.com>
2023-08-22 00:03:39 +0530

    Add files via upload

---
+++ Bomb.js
@@ -0,0 +1,21 @@
+let countElement =  document.getElementById("countChange");
+let enteredText = document.getElementById("enteredText");
+
+let count = 10;
+
+let countDecrease = setInterval(function(){
+    countElement.textContent = count;
+    count-=1;
+    if (count===0) {
+        countElement.textContent = "BOOM";
+        clearInterval(countDecrease);
+    }
+},2000);
+
+enteredText.addEventListener("keydown",function(event){
+    let enteredValue = enteredText.value;
+    if (enteredValue === "def" && event.key === "Enter" && count !==0){
+        countElement.textContent = "You did it!";
+    }
+});
+
